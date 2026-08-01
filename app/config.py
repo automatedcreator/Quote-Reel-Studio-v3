c4cc911b880aaa539a8e2288f137de99e514bdb7
automatedcreator <latestmoviz6677@gmail.com>
2026-08-01 09:14:12 +0530

    Add config module

---
+++ app/config.py
@@ -0,0 +1,20 @@
+"""
+Global Configuration
+"""
+
+WIDTH = 1080
+HEIGHT = 1920
+
+FPS = 30
+
+FONT_DIR = "assets/fonts"
+
+VIDEO_DIR = "videos"
+
+QUOTE_DIR = "quotes"
+
+OUTPUT_DIR = "output"
+
+TEMP_DIR = "temp"
+
+DEFAULT_THEME = "poetry"
