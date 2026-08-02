76ecb0edc1f80e16136db943ba39072446733e57
automatedcreator <latestmoviz6677@gmail.com>
2026-08-02 11:22:59 +0530

    Improved config file

--- app/config.py
+++ app/config.py
@@ -1,13 +1,40 @@
 """
+Quote Reel Studio v1.0
 Global Configuration
 """
+
+# -----------------------------
+# Video Settings
+# -----------------------------
 
 WIDTH = 1080
 HEIGHT = 1920
 
 FPS = 30
 
+VIDEO_BITRATE = "5000k"
+
+OUTPUT_FORMAT = "mp4"
+
+# -----------------------------
+# Typography
+# -----------------------------
+
 FONT_DIR = "assets/fonts"
+
+FONT_SIZE = 72
+
+LINE_SPACING = 15
+
+TEXT_COLOR = "white"
+
+SHADOW_COLOR = "black"
+
+MAX_TEXT_WIDTH = 850
+
+# -----------------------------
+# Directories
+# -----------------------------
 
 VIDEO_DIR = "videos"
 
@@ -17,4 +44,24 @@ OUTPUT_DIR = "output"
 
 TEMP_DIR = "temp"
 
+# -----------------------------
+# Theme
+# -----------------------------
+
 DEFAULT_THEME = "poetry"
+
+# -----------------------------
+# Export
+# -----------------------------
+
+OUTPUT_NAME = "reel"
+
+ZIP_NAME = "reels.zip"
+
+# -----------------------------
+# UI
+# -----------------------------
+
+APP_NAME = "Quote Reel Studio"
+
+VERSION = "1.0"
